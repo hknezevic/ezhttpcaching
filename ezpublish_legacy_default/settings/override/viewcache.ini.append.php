f79dc831eb6b92f0f56cdf69ba8c083661f6a7b7
Web Summer Camp 2017 <websummercamp@netgen.hr>
2017-08-29 11:02:40 +0200

    Enable legacy smart view cache rules

---
+++ ezpublish_legacy_default/settings/override/viewcache.ini.append.php
@@ -0,0 +1,16 @@
+<?php /* #?ini charset="utf-8"?
+
+[ViewCacheSettings]
+SmartCacheClear=enabled
+
+[blog_post]
+DependentClassIdentifier[]
+DependentClassIdentifier[]=blog
+DependentClassIdentifier[]=blog_post
+DependentClassIdentifier[]=home_page
+
+ClearCacheMethod[]
+ClearCacheMethod[]=object
+ClearCacheMethod[]=parent
+ClearCacheMethod[]=siblings
+ClearCacheMethod[]=relating
