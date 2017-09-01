2e27925304d6a57db034eaf9737edcd4db298dc4
Web Summer Camp 2017 <websummercamp@netgen.hr>
2017-09-01 02:15:10 +0200

    Add prepare bash script

---
+++ prepare.sh
@@ -0,0 +1,8 @@
+cd /var/www/html/summercamp/ezhttpcaching
+cp ezpublish_legacy_default/config.php ezpublish_legacy/.
+cp -R ezpublish_legacy_default/settings/siteaccess/admin ezpublish_legacy/settings/siteaccess/.
+mkdir ezpublish_legacy/settings/override
+cp ezpublish_legacy_default/settings/override/site.ini.append.php ezpublish_legacy/settings/override/.
+sudo mv /etc/varnish/default.vcl /etc/varnish/default.vcl.backup
+sudo cp doc/websc/varnish.vcl /etc/varnish/default.vcl
+sudo service varnish restart
